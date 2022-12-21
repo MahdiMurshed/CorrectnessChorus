e8026d4f1baca8912754fce02e462b1d19ee3d15
MahdiMurshed <123mahdi12345@gmail.com>
2022-12-22 02:48:46 +0600

    feat: add basic grammar corrector

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
+import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { MantineProvider } from '@mantine/core';
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
+
+import { ToastContainer } from 'react-toastify';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   // eslint-disable-next-line no-unused-vars
@@ -17,6 +20,18 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   return getLayout(
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <Component {...pageProps} />;
+      <ToastContainer
+        position="bottom-right"
+        theme="dark"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </MantineProvider>
   );
 }
