924e88caefad57f01e6b0e2dfc508330b6f17221
nowshin alam owishi <owishibooo@pop-os.localdomain>
2023-02-01 02:01:42 +0600

    refactor: backnd fixed

--- pages/api/server.py
+++ pages/api/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 from happytransformer import HappyTextToText, TTSettings
 
 happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
@@ -7,12 +7,14 @@ args = TTSettings(num_beams=5, min_length=1)
 
 # Add the prefix "grammar: " before each input 
 result = happy_tt.generate_text("grammar: This sentences has has bads grammar.", args=args)
-
-print(result.text) # This sentence has bad grammar.
-app = Flask(__name__)
-@app.route("/members") #route
-def members():
-    return {"result":[result]}
-
-if __name__ == "__main__":
+app =   Flask(__name__)
+  
+@app.route('/members', methods = ['GET'])
+def ReturnJSON():
+    if(request.method == 'GET'):
+        data = jsonify(result)
+        data.headers.add("Access-Control-Allow-Origin", "*")
+        return data
+  
+if __name__=='__main__':
     app.run(debug=True)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
 import { HeroBullets } from '@components/hero';
+//import axios from 'axios';
 
+// const fetching = async () => {
+//   const response = await axios.get('http://127.0.0.1:5000/members');
+//   return response;
+// };
 export default function Home() {
+  // const res = fetching();
+  // console.log({ res });
   return <HeroBullets />;
 }
