c503435efe57471f393fd6a4e07fbfb985623a29
nowshin alam owishi <owishibooo@pop-os.localdomain>
2023-02-04 07:34:41 +0600

    build: changed to env

--- pages/api/server.py
+++ pages/api/server.py
@@ -1,12 +1,15 @@
 import json
 from flask import Flask,jsonify,request
+import os
+from dotenv import load_dotenv
+load_dotenv()  # this will load variables from .env.
 # from transformers import pipeline
 from happytransformer import HappyTextToText, TTSettings
 from flask_cors import CORS, cross_origin
 app =   Flask(__name__)
 CORS(app)
 # corrector = pipeline('text2text-generation','pszemraj/flan-t5-large-grammar-synthesis',)
-happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
+happy_tt = HappyTextToText("T5",os.environ.get("CORR_CHOR"))
 args = TTSettings(num_beams=5, min_length=1)
 @app.route('/members', methods = ['POST'])
 @cross_origin()
